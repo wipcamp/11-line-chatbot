f10e1dc45ebae48854fb25c59d81c4b454d4686c
pea <pepea.23@live.com>
2019-01-16 18:27:52 +0700

    [UPDATE] get jwt cookie

--- index.js
+++ index.js
@@ -1,99 +1,15 @@
-const express = require('express')
-const port = process.env.PORT || 9999
-const TOKEN = process.env.TOKEN || null
-const LOCAL = process.env.LOCAL || null
+const express = require('express');
+const axios = require('axios');
+const app = express();
 const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-const axios = require('axios')
+const port = process.env.PORT || 6000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.post('/test',(res,req)=> {
-    console.log('dsfd',req)
+app.post('/webhook',(res,req) =>{
+console.log('in')
 })
-    app.post('/webhook', async (req, res) => {
-        let reply_token = req.body.events[0].replyToken
-        let msg = req.body.events[0].message.text
-        console.log( req.body.events[0].source.userId)
-        // const questions = ''
-        // if (msg === 'ตอบคำถาม') {
-        //     try {
-        //         questions = await axios('http://127.0.0.1:8001/api/questions')
-        //     }catch(e){
-        //         console.log('Errrrrr Q  ',e)
-        //     }
-        // }
-        // console.log('question: '+questions)
-        // await axios.get('http://127.0.0.1:8001/api/questions')
-        // .then(function(response){
-        //     console.log('data : '+response.data); // ex.: { user: 'Your User'}
-        //     console.log('status : '+response.status); // ex.: 200
-        // });  
-    reply(reply_token, req.body.events[0].source.userId, msg)
-   
-    res.sendStatus(200)
-})
-app.get('/', (req, res) => res.sendStatus(200))
-app.listen(port)
 
-function reply(reply_token, path, msg, question) {
+app.listen(port);
 
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${TOKEN}`
-    }
-    let body
-    if (msg === 'connect') {
-        body = JSON.stringify({
-            replyToken: reply_token,
-            messages: [
-                {
-                    type: "flex",
-                    altText: "Connect hi",
-                    contents: {
-                        type: "bubble",
-                        body: {
-                            type: "box",
-                            layout: "vertical",
-                            contents: [
-                                {
-                                    type: "button",
-                                    style: "primary",
-                                    height: "sm",
-                                    action: {
-                                        type: "uri",
-                                        label: "Connect Wipcamp",
-                                        uri: `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1638650000&redirect_uri=https://localhost:3000&state=asdasd&scope=openid%20profile`
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                }
-            ]
-        })
-    } else if (msg == "ตอบคำถาม") {
-        body = JSON.stringify({
-            replyToken: reply_token,
-            messages: [{
-                type: 'text',
-                text: 'คำถามมี'
-            }
-            ]
-        })
-    }
-
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
-}
-
-function login(userId, accessToken) {
-    // U60a8501e03cd8ab7db37a5f7036aa417
-}
+console.log('App is listening on port ' + port);
